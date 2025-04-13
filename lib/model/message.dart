c03f8c72f4847e541de5066d6b9a6e483e09d1bc
SyedMuhammadRehan <syedrehan55.rs@gmail.com>
2025-04-13 15:21:16 +0500

    Update Message model to support formattingRanges

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -1,9 +1,17 @@
 import 'dart:typed_data';
+import 'package:paste_snap_demo/model/formatting_range.dart';
 
 class Message {
+  final String id;
+  final String? text;
   final Uint8List? imageData;
   final String timestamp;
-  final String? text;
+  final List<FormattingRange> formattingRanges;
 
-  Message(this.text, {this.imageData}) : timestamp = DateTime.now().toIso8601String();
+  Message(
+    this.text, {
+    this.imageData,
+    this.formattingRanges = const [],
+  })  : id = DateTime.now().millisecondsSinceEpoch.toString(),
+        timestamp = DateTime.now().toIso8601String();
 }
